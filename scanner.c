ff895f0ff2b2696c1967267dec88e2e001a0719c
user <user@debian.debian>
2025-02-17 09:51:55 +0530

    Reformated the code

--- scanner.c
+++ scanner.c
@@ -1,7 +1,8 @@
 #include "scanner.h"
 #include <ctype.h>
 
-const char *token_to_str(TokenType type){
+const char *token_to_str(TokenType type)
+{
 
     #define TOKEN(name) #name,
     #define TOKEN_EXCLUDE(name)
@@ -11,21 +12,28 @@ const char *token_to_str(TokenType type){
 
     #undef TOKEN_EXCLUDE
     #undef TOKEN
-    if(type>=total_tokens){
+
+    if(type>=total_tokens)
+    {
         return NULL; 
     }
+
     return tokens[type];
 
 }
 
-bool _token_is(TokenType token_type, ...){ // similar to in python (token in (INT,STRING....))
+ // similar to in python (token in (INT,STRING....))
+bool _token_is(TokenType token_type, ...)
+{
     va_list args;
     va_start(args, token_type);
 
     TokenType tmp;
     bool result = false;
-    while ((tmp = va_arg(args, TokenType)) != UNKNOWN) {
-        if (tmp == token_type) {
+    while ((tmp = va_arg(args, TokenType)) != UNKNOWN)
+    {
+        if (tmp == token_type)
+        {
             result = true;
             break;            
         }
@@ -36,20 +44,32 @@ bool _token_is(TokenType token_type, ...){ // similar to in python (token in (IN
 }
 
 
-inline void print_token(Token token){
-    printf("Token(%s, %d, \"%s\")\n", token_to_str(token.type), token.line,
-           token.lexeme);
+inline void print_token(Token token)
+{
+    printf("Token(%s, %d, \"%s\")\n",
+            token_to_str(token.type),
+            token.line,
+            token.lexeme
+          );
 }
 
-static inline bool is_at_end(ScannerState *ss){ return ss->current >= ss->source_len; }
+static inline bool is_at_end(ScannerState *ss)
+{ 
+    return ss->current >= ss->source_len; 
+}
 
-static inline char advance(ScannerState *ss){ return ss->source[ss->current++]; }
+static inline char advance(ScannerState *ss)
+{ 
+    return ss->source[ss->current++];
+}
 
-static inline char peek(ScannerState *ss){
+static inline char peek(ScannerState *ss)
+{
     return is_at_end(ss) ? '\0' : ss->source[ss->current];
 }
 
-static inline bool match(char expected, ScannerState *ss){
+static inline bool match(char expected, ScannerState *ss)
+{
     if (is_at_end(ss) || ss->source[ss->current] != expected)
         return false;
 
@@ -57,36 +77,45 @@ static inline bool match(char expected, ScannerState *ss){
     return true;
 }
 
-static char *substr(const char *source, int start, int end){
-    if (start < 0 || start > end || !source){
+static char *substr(const char *source, int start, int end)
+{
+    if (start < 0 || start > end || !source)
+    {
         return NULL;
     }
 
     int substr_size = end - start;
     char *result = calloc(substr_size + 1, 1);
-    if (!result){
-        fprintf(stderr,"Error: Unable to allocate memory\n");
-        exit(1);
+
+    if (!result)
+    {
+        return NULL;
     }
+
     strncpy(result, &source[start], substr_size);
     result[substr_size] = '\0';
+
     return result;
 }
 
 
-static char peek_next(ScannerState *ss){
+static char peek_next(ScannerState *ss)
+{
     if (ss->current + 1 >= ss->source_len)
         return '\0';
     return ss->source[ss->current + 1];
 }
 
-static Token string(ScannerState *ss){
-    while (peek(ss) != '"' && !is_at_end(ss)){
+static Token string(ScannerState *ss)
+{
+    while (peek(ss) != '"' && !is_at_end(ss))
+    {
         if (peek(ss) == '\n')
             ss->line++;
         advance(ss);
     }
-    if (is_at_end(ss)){
+    if (is_at_end(ss))
+    {
         invalid_token("Error: %s","Unterminated string.\n");
         exit(-1);
     }
@@ -101,12 +130,16 @@ static Token string(ScannerState *ss){
     return token;
 }
 
-static inline bool is_octal_digit(char c){
+static inline bool is_octal_digit(char c)
+{
     if(c >= '0' && c <= '7') return true;
     return false;
 }
-static Token octal_number(ScannerState *ss){
-    while(is_octal_digit(peek(ss))){
+
+static Token octal_number(ScannerState *ss)
+{
+    while(is_octal_digit(peek(ss)))
+    {
          advance(ss);
     }
 
@@ -121,24 +154,34 @@ static Token octal_number(ScannerState *ss){
     return token;
 }
 
-bool is_valid_hex_digit(char c) {
-    if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') ||
-        (c >= 'A' && c <= 'F')){
+bool is_valid_hex_digit(char c)
+{
+    if ((c >= '0' && c <= '9') ||
+        (c >= 'a' && c <= 'f') ||
+        (c >= 'A' && c <= 'F'))
+    {
 
-            return true;
+        return true;
 
-        }
+    }
     return false;
 }
-static Token hex_number(ScannerState *ss){
-    while(is_valid_hex_digit(peek(ss))){
+
+static Token hex_number(ScannerState *ss)
+{
+    while(is_valid_hex_digit(peek(ss)))
+    {
         advance(ss);
     }
 
     char *lexeme = substr(ss->source,ss->start, ss->current); 
-    if(strlen(lexeme) == 2){ // lexeme only contains only 0x 
+
+     // lexeme only contains only 0x 
+    if(strlen(lexeme) == 2)
+    {
         exit(-1);
     }
+
     Token token;
     token.type   = HEX_CONSTANT;
     token.line   = ss->line;
@@ -149,15 +192,21 @@ static Token hex_number(ScannerState *ss){
     return token;
 }
 
-static Token number_decimal(ScannerState *ss){
+static Token number_decimal(ScannerState *ss)
+{
     bool is_float = false;
-    while (isdigit(peek(ss))){
+    while (isdigit(peek(ss)))
+    {
         advance(ss);
     }
-    if (peek(ss) == '.' && isdigit(peek_next(ss))){
+
+    if (peek(ss) == '.' && isdigit(peek_next(ss)))
+    {
         is_float = true;
         advance(ss);
-        while (isdigit(peek(ss))){
+
+        while (isdigit(peek(ss)))
+        {
             advance(ss);
         }
     }
@@ -170,50 +219,44 @@ static Token number_decimal(ScannerState *ss){
     ss->start    = ss->current;
 
     return token;
-                  
 }
 
-static Token number(char first_digit , ScannerState *ss) {
-    if (first_digit == '0') {
+static Token number(char first_digit , ScannerState *ss)
+{
+    if (first_digit == '0')
+    {
         char next = peek(ss);
-        if (next == 'X' || next == 'x') {
+        if (next == 'X' || next == 'x')
+        {
             advance(ss); // consume x or X
             return hex_number(ss);
         }
+
         return octal_number(ss);
     }
 
     return number_decimal(ss);
 }
 
-static TokenType keyword(const char *identifier){
+static TokenType keyword(const char *identifier)
+{
 #define STRCMP(str) strcmp(identifier, str) == 0
-    if (STRCMP("or")){
-        return OR;
-    } else if (STRCMP("for")){
-        return FOR;
-    } else if (STRCMP("while")){
-        return WHILE;
-    } else if (STRCMP("return")){
-        return RETURN;
-    } else if (STRCMP("else")){
-        return ELSE;
-    } else if (STRCMP("true")){
-        return TRUE;
-    } else if (STRCMP("false")){
-        return FALSE;
-    } else if (STRCMP("if")){
-        return IF;
-    } else if (STRCMP("and")){
-        return AND;
-    } else if (STRCMP("fn")){
-        return FUNCTION;
-    } else {
-        return IDENTIFYER;
-    }
+
+    if (STRCMP("or"))          return OR;
+    else if (STRCMP("for"))    return FOR;
+    else if (STRCMP("while"))  return WHILE;
+    else if (STRCMP("return")) return RETURN;
+    else if (STRCMP("else"))   return ELSE;
+    else if (STRCMP("true"))   return TRUE;
+    else if (STRCMP("false"))  return FALSE;
+    else if (STRCMP("if"))     return IF;
+    else if (STRCMP("and"))    return AND;
+    else if (STRCMP("fn"))     return FUNCTION;
+    else                       return IDENTIFYER;
 }
 
-static Token identifier(ScannerState *ss){
+static Token identifier(ScannerState *ss)
+{
     while (isalnum(peek(ss)) || peek(ss) == '_')
         advance(ss);
 
@@ -229,7 +272,8 @@ static Token identifier(ScannerState *ss){
     return token;
 } 
 
-static Token build_token(TokenType token_type , ScannerState *ss){
+static Token build_token(TokenType token_type , ScannerState *ss)
+{
     Token token;
     token.type   = token_type;
     token.line   = ss->line;
@@ -240,10 +284,13 @@ static Token build_token(TokenType token_type , ScannerState *ss){
     return token;
 }
 
-Token next_token(ScannerState *ss) {
-    while (!is_at_end(ss)) {
+Token next_token(ScannerState *ss)
+{
+    while (!is_at_end(ss))
+    {
         char c = advance(ss);
-        switch (c) {
+        switch (c)
+        {
         case '%':
             return build_token(MOD, ss);
         case '*':
@@ -294,20 +341,20 @@ Token next_token(ScannerState *ss) {
             break;
         //     // while(!is_at_end(ss) && peek(ss)==' ') advance(ss);
         default:
-            if (isdigit(c)) return number(c,ss);
-            else if (isalpha(c) || c == '_'){
-                return identifier(ss);
-            }
-            else{
+            if (isdigit(c))                  return number(c,ss);
+            else if (isalpha(c) || c == '_') return identifier(ss);
+            else
+            {
                 invalid_token("Error: Unknown character %c\n",c);
                 exit(1);
             } 
         }
     }
+
     Token token;
     token.type   = FILE_END;
     token.line   = ss->line;
     token.lexeme = NULL;
+
     return token;
 }
-
